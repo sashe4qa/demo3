e4027fee6bba754dcc45cc441775428e8f924b9a
alexg <alexg@Vlads-MacBook-Pro.local>
2015-08-01 13:57:15 +0300

    Initial Commit

---
+++ 44_CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  44_CoreData
+//
+//  Created by alexg on 01.08.15.
+//  Copyright (c) 2015 alexg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 44_CoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  44_CoreData
+//
+//  Created by alexg on 01.08.15.
+//  Copyright (c) 2015 alexg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
